69e0c4e8655e323b9d2629301911b6fca908a8ba
davidteather <davidteather@users.noreply.github.com>
2020-04-20 23:43:15 -0500

    header fix

--- reformat.py
+++ reformat.py
@@ -6,7 +6,7 @@ with open("data.csv", 'r', encoding="utf-8") as inp:
     lines = inp.readlines()
 
     with open("reformat.csv", 'a', encoding="utf-8") as output:
-        output.write("City,Link,Content\n")
+        output.write("City\n")
         for x in range(1,int(lines[0].replace("\n", ""))):
             # iterate over lines
             output.write(lines[x].split("\t")[0].replace(",", "%2C") + " " + lines[x].split("\t")[1].split("/")[0].title().replace(",", "%2C") + "\n")
